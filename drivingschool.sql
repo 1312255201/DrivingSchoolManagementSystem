2a2696306cbbc0076ac9037316423c8374795e57
1312255201 <1312255201@qq.com>
2024-12-09 01:36:49 +0800

    更新数据库
    更新数据库

--- drivingschool.sql
+++ drivingschool.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726 (5.7.26-log)
  File Encoding         : 65001
 
- Date: 08/12/2024 20:11:01
+ Date: 09/12/2024 01:36:23
 */
 
 SET NAMES utf8mb4;
@@ -42,7 +42,7 @@ CREATE TABLE `course_selection`  (
   `course_id` int(11) NOT NULL COMMENT '课程ID',
   `selected_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '选课时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_student`(`student_id`) USING BTREE,
+  UNIQUE INDEX `kf_studentandcourse`(`student_id`, `course_id`) USING BTREE COMMENT '防止重复选课',
   INDEX `fk_course`(`course_id`) USING BTREE,
   CONSTRAINT `fk_course` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_student` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -65,6 +65,20 @@ CREATE TABLE `courses`  (
   INDEX `coach_id`(`coach_id`) USING BTREE,
   CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`coach_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '课程表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for email_config
+-- ----------------------------
+DROP TABLE IF EXISTS `email_config`;
+CREATE TABLE `email_config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `smtp_host` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'smtp.qq.com',
+  `smtp_port` int(11) NOT NULL DEFAULT 465,
+  `is_ssl_enabled` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for exam_selection
@@ -101,6 +115,21 @@ CREATE TABLE `exams`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '考试白哦' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for fees
+-- ----------------------------
+DROP TABLE IF EXISTS `fees`;
+CREATE TABLE `fees`  (
+  `fee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fee_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `amount` decimal(10, 2) NOT NULL,
+  `is_installment` tinyint(1) NULL DEFAULT 0,
+  `installment_count` int(11) NULL DEFAULT 0,
+  `payment_options` json NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`fee_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
 -- Table structure for leave_requests
 -- ----------------------------
 DROP TABLE IF EXISTS `leave_requests`;
@@ -116,10 +145,57 @@ CREATE TABLE `leave_requests`  (
   `admin_id` int(11) NULL DEFAULT NULL,
   `reviewed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `review_comments` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `evidence_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `coach_id`(`coach_id`) USING BTREE,
   CONSTRAINT `leave_requests_ibfk_1` FOREIGN KEY (`coach_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '请假记录表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '请假记录表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE `password_resets`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `token`(`token`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for payment_records
+-- ----------------------------
+DROP TABLE IF EXISTS `payment_records`;
+CREATE TABLE `payment_records`  (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `fee_id` int(11) NOT NULL,
+  `amount` decimal(10, 2) NOT NULL,
+  `payment_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `payment_method` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `payment_status` enum('成功','失败') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`payment_id`) USING BTREE,
+  INDEX `fee_id`(`fee_id`) USING BTREE,
+  CONSTRAINT `payment_records_ibfk_1` FOREIGN KEY (`fee_id`) REFERENCES `fees` (`fee_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for refunds
+-- ----------------------------
+DROP TABLE IF EXISTS `refunds`;
+CREATE TABLE `refunds`  (
+  `refund_id` int(11) NOT NULL AUTO_INCREMENT,
+  `payment_id` int(11) NOT NULL,
+  `refund_amount` decimal(10, 2) NOT NULL,
+  `refund_reason` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `refund_status` enum('处理中','已完成') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `refund_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`refund_id`) USING BTREE,
+  INDEX `payment_id`(`payment_id`) USING BTREE,
+  CONSTRAINT `refunds_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment_records` (`payment_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for schedule
@@ -163,6 +239,24 @@ CREATE TABLE `student_state`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for students
+-- ----------------------------
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE `students`  (
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `registration_status` enum('PENDING','APPROVED','REJECTED') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'PENDING',
+  `reject_reason` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `review_time` timestamp NULL DEFAULT NULL,
+  `reviewer_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`student_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
 -- Table structure for teach_info
 -- ----------------------------
 DROP TABLE IF EXISTS `teach_info`;
@@ -193,8 +287,8 @@ CREATE TABLE `users`  (
   `finish_info` enum('yes','no') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'no',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `email`(`email`) USING BTREE,
   UNIQUE INDEX `phonenumber`(`phonenumber`) USING BTREE,
+  UNIQUE INDEX `email`(`email`) USING BTREE,
   UNIQUE INDEX `idnumber`(`idnumber`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
